e952106b8981f910126c53ba5992e2956abd9b91
imamrakib354 <109544256+imamrakib354@users.noreply.github.com>
2022-07-19 11:08:03 +0600

    Add files via upload

---
+++ Lab 01/354 (49-08)  problem 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0 && n!=2)
+        cout<<"yes"<<endl;
+    else
+        cout<<"no"<<endl;
+
+    return 0;
+}
+
